fb18a6379096c54e356ba887b3a5c04251098579
kaya0s <erwin.lanzaderas@gmail.com>
2025-10-10 18:28:12 +0800

    login interface using shadcn components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
+import { StrictMode } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeToggle } from '@/components/theme-toggle'
+import LoginPage from '@/pages/login'
+import SignupPage from '@/pages/signup'
 
 function App() {
   return (
-    <>
-     <h1 className="text-red-700 font-bold ">
-      testing tailwindcss
-     </h1>
-      
-    </>
+    <StrictMode>
+      <ThemeProvider defaultTheme="light" storageKey="hj-events-theme">
+        <Router>
+          <div className="relative min-h-screen">
+            <div className="fixed top-4 right-4 z-50">
+              <ThemeToggle />
+            </div>
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </StrictMode>
   )
 }
 
