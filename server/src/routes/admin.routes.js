703fdffad6ed1998d8ec1090ab0f83ffdded03bb
kaya0s <erwin.lanzaderas@gmail.com>
2025-10-20 01:47:00 +0800

    add announcement controller and routes for posting and listing announcements

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { protect, isAdmin } from '../middlewares/auth.js';
 import { getDashboard, getMonthlyRevenue } from '../controllers/reports.controller.js';
-
+import { postAnnouncement,listAnnouncements,getActivityLogs } from '../controllers/announcement.controller.js';
 const router = express.Router();
 
 // Admin-only
 router.get('/dashboard', protect, isAdmin, getDashboard);
 router.get('/revenue', protect, isAdmin, getMonthlyRevenue);
 router.get('/activity', protect, isAdmin, getActivityLogs);
+router.post('/announcement',protect,isAdmin,postAnnouncement)
 
 // Public: announcements
 router.get('/announcements', listAnnouncements);
