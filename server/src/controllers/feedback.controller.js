2bd525d59a9115947f1fd149971b7132d21cea5f
kaya0s <erwin.lanzaderas@gmail.com>
2025-10-20 02:27:29 +0800

    todo

--- server/src/controllers/feedback.controller.js
+++
@@ -1,9 +0,0 @@
-import Feedback from '../models/feedback.model';
-import Supplier from '../models/supplier.model';
-
-export const leaveFeedback = async (req, res) => {
-  try {
-  } catch (error) {
-    const { supplierId, rating, comment, bookingId } = req.body;
-  }
-};
