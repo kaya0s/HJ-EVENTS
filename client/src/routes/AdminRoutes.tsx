b68e946cdf0c31fdf699e237bc9e1f6c6c49b7db
kaya0s <erwin.lanzaderas@gmail.com>
2025-11-04 16:44:44 +0800

    add routes for roles

--- client/src/routes/AdminRoutes.tsx
+++ client/src/routes/AdminRoutes.tsx
@@ -3,12 +3,16 @@ import AdminDashboard from "@/pages/admin/AdminDashboard";
 import AdminCalendar from "@/pages/admin/AdminCalendar";
 
 import { Bookings } from "@/pages/admin/AdminBookings";
+import Supplier from "@/pages/admin/Supplier";
+import PackageManagement from "@/pages/admin/PackageManagement";
 
 const AdminRoutes = () => (
   <Routes>
     <Route path="/admin" element={<AdminDashboard />} />
     <Route path="/admin/bookings" element={<Bookings />} />
     <Route path="/admin/calendar" element={<AdminCalendar />} />
+    <Route path="/admin/suppliers" element={<Supplier />} />
+    <Route path="/admin/packages" element={<PackageManagement />} />
   </Routes>
 );
 export default AdminRoutes;
