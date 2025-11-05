05599e88e342d7a16a2b1cec9d7c0c880e724e9e
kaya0s <erwin.lanzaderas@gmail.com>
2025-11-05 19:44:05 +0800

    client setup tailwind+daisyui

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <div className="avatar">
+        <div className="w-32 rounded">
+          <img src="https://img.daisyui.com/images/profile/demo/superperson@192.webp" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-20 rounded">
+          <img
+            src="https://img.daisyui.com/images/profile/demo/superperson@192.webp"
+            alt="Tailwind-CSS-Avatar-component"
+          />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-16 rounded">
+          <img
+            src="https://img.daisyui.com/images/profile/demo/superperson@192.webp"
+            alt="Tailwind-CSS-Avatar-component"
+          />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-8 rounded">
+          <img
+            src="https://img.daisyui.com/images/profile/demo/superperson@192.webp"
+            alt="Tailwind-CSS-Avatar-component"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
