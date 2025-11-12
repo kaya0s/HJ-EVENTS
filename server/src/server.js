f87dd693c2e422ee87efab0eb2151641cf83dcae
kaya0s <erwin.lanzaderas@gmail.com>
2025-11-13 03:43:44 +0800

    fixed file path

--- server/src/server.js
+++ server/src/server.js
@@ -64,10 +64,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 // Start server
